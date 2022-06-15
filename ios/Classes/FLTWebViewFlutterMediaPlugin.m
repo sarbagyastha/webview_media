6e951dee79bf9dc27f0b1313c62cd68358ad47d2
Rodrigo <rodrigo.miyashiro@yahoo.com.br>
2022-06-14 21:49:39 -0300

    wip

--- ios/Classes/FLTWebViewFlutterMediaPlugin.m
+++ ios/Classes/FLTWebViewFlutterMediaPlugin.m
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "FLTWebViewFlutterPlugin.h"
+#import "FLTWebViewFlutterMediaPlugin.h"
 #import "FLTCookieManager.h"
 #import "FlutterWebView.h"
 
-@implementation FLTWebViewFlutterPlugin
+@implementation FLTWebViewFlutterMediaPlugin
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FLTWebViewFactory* webviewFactory =
